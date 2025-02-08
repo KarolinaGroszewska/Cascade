17d2d051c6c9f0bf242571e9c91c22fcfddd03f5
Karolina Groszewska <karolina.groszewska@gmail.com>
2025-02-08 17:28:45 -0600

    Initial Commit

---
+++ Cascade/CascadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CascadeApp.swift
+//  Cascade
+//
+//  Created by Kari Groszewska on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CascadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
